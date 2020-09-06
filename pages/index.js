68e6ff8065080c877a287468f8ad8fd885f1395f
kang <minhyupp@gmail.com>
2020-09-07 06:33:13 +0900

    최신화

--- pages/index.js
+++ pages/index.js
@@ -44,6 +44,13 @@ export default function Home() {
       <div className="test">메인페이지 입니다.</div>
       <div className="hover"></div>
       <img src="/hor.jpg" alt="해오름 Logo" />
+      <video
+        className=""
+        src="https://player.vimeo.com/external/418321067.sd.mp4?s=a6dce4e09cdc909847a902c7054f45e4ecf5c01e&amp;profile_id=165"
+        poster="https://dev.does.kr/2017/1703_picnic/_resource/_web/images/service/detail/pet01_video_poster.jpg"
+        controls
+        playsInline
+      ></video>
       <button onClick={test}>test</button>
     </MenuLayout>
   );
